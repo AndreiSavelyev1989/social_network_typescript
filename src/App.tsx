e93863fe71692416ca1df03a2e20be6cdbc26982
Andrei <andreiscorpion19892010@gmail.com>
2020-08-26 22:42:02 +0200

    add data to DialogsComponent

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,15 @@ import {Settings} from "./components/Settings/Settings";
 import {Profile} from "./components/Profile/Profile";
 import {BrowserRouter, Route} from "react-router-dom";
 
+
+const dialogs = [
+    {id: 1, name: "Andrei", message: "Hello people!"},
+    {id: 2, name: "Pavel", message: "Welcome!"},
+    {id: 3, name: "Viktor", message: "How are you?"},
+    {id: 4, name: "Dima", message: "Nice to meet you!"},
+    {id: 5, name: "Sveta", message: "What a wonderful day!"}
+]
+
 function App() {
     return (
         <BrowserRouter>
@@ -17,7 +26,7 @@ function App() {
                 <Navbar/>
                 <div className="app_wrapper_content">
                     <Route  path = '/profile' render = {() => <Profile />}/>
-                    <Route  path = '/dialogs' render = {() => <Dialogs />}/>
+                    <Route  path = '/dialogs' render = {() => <Dialogs dialogs = {dialogs}/>}/>
                     <Route  path = '/news' render = {News}/>
                     <Route  path = '/music' render = {Music}/>
                     <Route  path = '/settings' render = {Settings}/>
--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import styles from "./Dialogs.module.css"
+import {NavLink} from "react-router-dom";
 
-export function Dialogs() {
+type DialogsPropsType = {
+    id: number
+    name: string
+    message: string
+}
+type DialogsType = {
+    dialogs: Array<DialogsPropsType>
+}
+
+export function Dialogs(props:DialogsType) {
     return (
-        <div>Dialogs</div>
+        <div className={styles.dialogs}>
+            <div className={styles.dialogItem}>
+                {props.dialogs.map(d => <div>
+                    <NavLink to={"/dialogs"}>{d.name}</NavLink>
+                </div>)}
+            </div>
+            <div className={styles.messageItem}>
+                {props.dialogs.map(d => <div>{d.message}</div>)}
+            </div>
+        </div>
     )
 }
