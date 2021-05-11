7b3750d474b222cce3affce4f471962f0e43c963
Andrei <andreiscorpion19892010@gmail.com>
2021-05-11 21:40:29 +0300

    refactored path get userProfile

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,12 @@
 import React, {useEffect, useState} from "react";
 import styles from "./Header.module.scss"
-import mainLogo from "../../images/main_logo.png"
 import {AuthMe} from "./auth-me/AuthMe";
 import {useDispatch, useSelector} from "react-redux";
 import {StoreType} from "../../redux-state/redux-store";
 import {authMe, AuthUserType} from "../../redux-state/auth-reducer";
 import {ProfileType} from "../../redux-state/profile-reducer";
 import {Navbar} from "../navbar/Navbar";
-import {TiSocialDribbble, RiMenu3Line} from "react-icons/all";
+import {RiMenu3Line, TiSocialDribbble} from "react-icons/all";
 import {IconContext} from "react-icons/lib";
 import {NavLink} from "react-router-dom";
 
@@ -16,7 +15,10 @@ export const Header = React.memo(() => {
 
     const dispatch = useDispatch()
     const {isAuth, login, isLoggedIn, error} = useSelector<StoreType, AuthUserType>(state => state.auth)
-    const userProfile = useSelector<StoreType, ProfileType | null>(state => state.auth.authProfile)
+    const userProfile = useSelector<StoreType, ProfileType | null>(state => {
+        const {profile} = state.profilePage;
+        return profile;
+    })
     const [loginSidebar, setLoginSidebar] = useState(false)
     const showLoginSidebar = () => setLoginSidebar(!loginSidebar)
 
